1fb68950066cc6404b7ac9215d173fa3ff1a2a44
Ace Jordan <acelumaad@live.com>
2019-02-11 21:55:25 +0800

    updates

--- app/models/item.go
+++ app/models/item.go
@@ -67,8 +67,22 @@ func GetItem(db *sqlx.DB, id int) (Item, error) {
 //StoreItem create new item
 func StoreItem(db *sqlx.DB, item *Item) (int64, error) {
 
-	insertItem := `INSERT INTO items (item, description, stock_code, barcode, category_id) VALUES ($1, $2, $3, $4, $5) RETURNING id`
-	_, err := db.Exec(insertItem, item.Item, item.Description, item.StockCode, item.Barcode, item.CategoryID)
+	// ID               int     `json:"id" db:"id"`
+	// Item             string  `json:"item" db:"item"`
+	// Description      string  `json:"description" db:"description"`
+	// StockCode        string  `json:"stock_code" db:"stock_code"`
+	// Barcode          string  `json:"barcode" db:"barcode"`
+	// CategoryID       int     `json:"category_id" db:"category_id"`
+	// UOM              string  `json:"uom" db:"uom"`
+	// Packaging        string  `json:"packaging" db:"packaging"`
+	// PackageQty       int     `json:"package_qty" db:"package_qty"`
+	// MarkupPercent    float64 `json:"markup_percent" db:"markup_percent"`
+	// MarkupAmount     float64 `json:"markup_amount" db:"markup_amount"`
+	// StockNotifyLimit int     `json:"stock_notify_limit" db:"stock_notify_limit"`
+	// StockLimit       int     `json:"stock_limit" db:"stock_limit"`
+
+	insertItem := `INSERT INTO items (item, description, stock_code, barcode, category_id, uom, packaging, package_qty, markup_percent, markup_amount, stock_notify_limit, stock_limit) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING id`
+	_, err := db.Exec(insertItem, item.Item, item.Description, item.StockCode, item.Barcode, item.CategoryID, item.UOM, item.Packaging, item.PackageQty, item.MarkupPercent, item.MarkupAmount, item.StockNotifyLimit, item.StockLimit)
 
 	if err != nil {
 		return 404, err
